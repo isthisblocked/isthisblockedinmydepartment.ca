6aff0c9d39285115a1132cc4a4fb755d03a2ee53
Sean Boots <sean.boots@gmail.com>
2018-10-11 22:13:34 -0400

    Removed H1 from header title

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,9 @@ import styles from '../styles/sass.module.scss'
 const Header = ({ siteTitle }) => (
   <div className={styles.headerContainer}>
     <div className={styles.container}>
-      <h1 style={{ margin: 0 }}>
+      <div className={styles.headerTitle}>
         <Link to="/">{siteTitle}</Link>
-      </h1>
+      </div>
     </div>
   </div>
 )
