8c0cd17a736e5b43ccab90f491990d44a7527671
Sean Boots <sean.boots@gmail.com>
2018-10-21 17:30:47 -0400

    Updates to the webmanifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Is this blocked in my department.ca',
+        short_name: 'Is this blocked',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/itb-logo.png', // This path is relative to the root of the site.
       },
     },
     {
